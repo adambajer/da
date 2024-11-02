19719c36663f173b45ecf3ff1724c00309f15f82
adambajer <adam.bajer@gmail.com>
2024-11-02 10:44:52 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,6 @@
-// Initialize Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+    databaseURL: "https://voice-noter-default-rtdb.europe-west1.firebasedatabase.app",
 };
-
-// Replace the placeholder values with your actual Firebase project configuration
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
