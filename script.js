46b9ea927052e2b1821c5b6a946d2725b152ca44
adambajer <adam.bajer@gmail.com>
2024-11-02 20:22:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const firebaseConfig = {
     databaseURL: "https://voice-noter-default-rtdb.europe-west1.firebasedatabase.app",
 };
 firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
 // Get references to DOM elements
 // Get references to DOM elements
 const canvas = document.getElementById('drawingCanvas');
