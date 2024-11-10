ee6b48071bbe62b68a9327b33ca54d2542376339
adambajer <adam.bajer@gmail.com>
2024-11-10 20:53:50 +0100

    a
    a

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ const maxHistorySteps = 500;
 const toggleGridButton = document.getElementById('toggleGridButton');
 toggleSnapButton.id = 'toggleSnapButton';
 toggleSnapButton.style.display = 'none';
-toggleSnapButton.textContent = 'Snap OFF';
+toggleSnapButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-240q-100 0-170-70T40-480q0-100 70-170t170-70h400q100 0 170 70t70 170q0 100-70 170t-170 70H280Zm0-80h400q66 0 113-47t47-113q0-66-47-113t-113-47H280q-66 0-113 47t-47 113q0 66 47 113t113 47Zm0-40q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm200-120Z"/></svg>';
 document.body.appendChild(toggleSnapButton);
 
 let snapToGridEnabled = false;
@@ -85,7 +85,7 @@ toggleGridButton.addEventListener('click', () => {
 // Event listener to toggle snap-to-grid functionality
 toggleSnapButton.addEventListener('click', () => {
   snapToGridEnabled = !snapToGridEnabled;
-  toggleSnapButton.textContent = snapToGridEnabled ? 'Snap ON' : 'Snap OFF';
+  toggleSnapButton.innerHTML = snapToGridEnabled ? '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-240q-100 0-170-70T40-480q0-100 70-170t170-70h400q100 0 170 70t70 170q0 100-70 170t-170 70H280Zm0-80h400q66 0 113-47t47-113q0-66-47-113t-113-47H280q-66 0-113 47t-47 113q0 66 47 113t113 47Zm400-40q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM480-480Z"/></svg>' : '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-240q-100 0-170-70T40-480q0-100 70-170t170-70h400q100 0 170 70t70 170q0 100-70 170t-170 70H280Zm0-80h400q66 0 113-47t47-113q0-66-47-113t-113-47H280q-66 0-113 47t-47 113q0 66 47 113t113 47Zm0-40q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm200-120Z"/></svg>';
 });
 // Mouse Events
 canvas.addEventListener('mousedown', (e) => {
